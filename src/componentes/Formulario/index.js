84f2a83b06d691e480f62fe443faf8ecd5afc6f3
birajnior <ubiratanbernardo@gmail.com>
2023-12-10 16:32:17 -0300

    Criando os primeiros componentes

---
+++ src/componentes/Formulario/index.js
@@ -0,0 +1,16 @@
+import './Formulario.css';
+import CampoTexto from '../CampoTexto/index.js';
+
+const Formulario = () => {
+  return (
+    <section className='formulario'>
+      <form>
+        <h2>Preencha os dados para criar o card do colaborador.</h2>
+        <CampoTexto label="Nome" placeholder="Digite seu nome" />
+        <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
+        <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem" />
+      </form>
+    </section>
+  )
+};
+export default Formulario
