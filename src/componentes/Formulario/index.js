a0285c0b846f6983dda12597609e90e856a8142c
birajnior <ubiratanbernardo@gmail.com>
2023-12-11 12:07:54 -0300

    Criando Lista e Botão

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,16 +1,35 @@
-import './Formulario.css';
-import CampoTexto from '../CampoTexto/index.js';
+import "./Formulario.css";
+import CampoTexto from "../CampoTexto/index.js";
+import ListaSuspensa from "../ListaSuspensa/index.js";
+import Botao from "../Botao/index.js";
 
 const Formulario = () => {
+  const times = [
+    "Programação",
+    "Front End",
+    "Data Science",
+    "Devops",
+    "UX e Design",
+    "Mobile",
+    "Inovação e Gestão",
+  ];
+
+  const aoSalvar = (evento) => {
+    evento.preventDefault();
+    console.log("Form foi submetido");
+  };
+
   return (
-    <section className='formulario'>
-      <form>
+    <section className="formulario">
+      <form onSubmit={aoSalvar}>
         <h2>Preencha os dados para criar o card do colaborador.</h2>
-        <CampoTexto label="Nome" placeholder="Digite seu nome" />
-        <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
-        <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem" />
+        <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome" />
+        <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo" />
+        <CampoTexto obrigatorio={false} label="Imagem" placeholder="Informe o endereço da imagem" />
+        <ListaSuspensa obrigatorio={true} label="Time" itens={times} />
+        <Botao>Criar card</Botao>
       </form>
     </section>
-  )
+  );
 };
-export default Formulario
+export default Formulario;
